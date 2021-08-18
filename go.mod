3f8360ff637e18a114324222962f651feddae360
DapperBlondie <alirezagharib110@gmail.com>
2021-08-18 23:51:56 +0430

    basic server implemented

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/DapperBlondie/websocket-demonstration
 
 go 1.16
+
+require (
+	github.com/rs/zerolog v1.23.0
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d
+)
--- src/cmd/server/server-main.go
+++ src/cmd/server/server-main.go
@@ -1,7 +1,35 @@
 package main
 
+import (
+	"fmt"
+	_ "github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/net/websocket"
+	"net/http"
+	"strconv"
+)
+
 func main() {
-
-
+	http.Handle("/", websocket.Handler(Echo))
 	return
+}
+
+func Echo(ws *websocket.Conn) {
+	fmt.Println("We are starting echoing ...")
+
+	for j := 0; j < 10; j += 1 {
+		msg := "Hello" + " " + strconv.Itoa(j)
+		err := websocket.Message.Send(ws, msg)
+		if err != nil {
+			log.Error().Msg(err.Error() + "; Error in sending msg")
+			return
+		}
+		var reply string
+		err = websocket.Message.Receive(ws, &reply)
+		if err != nil {
+			log.Error().Msg(err.Error() + "; Error in sending msg")
+			return
+		}
+		fmt.Println("Client said : " + reply)
+	}
 }
